79b70a9dfc696208f8ef99f131803e33c2f380e6
cartershanklin <carter@cartershanklin.com>
2013-12-11 10:03:03 -0800

    Fixed exiting when files not found.

--- build.sh
+++ build.sh
@@ -2,7 +2,12 @@
 
 # Check for all the stuff I need to function.
 for f in gcc flex bison mvn; do
-	which $f > /dev/null 2>&1 || ( echo "Required program $f is missing. Please install it and try again." ; exit 1 )
+	which $f > /dev/null 2>&1
+	if [ $? -ne 0 ]; then
+		echo "Required program $f is missing. Please install it and try again."
+		exit 1
+	fi
 done
 
+echo "Building Data Generator"
 (cd tpcds-gen; make)
