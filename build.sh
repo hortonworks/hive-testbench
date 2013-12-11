6545546cba4fd5b0c2322c81e88df29e2f71df2a
cartershanklin <carter@cartershanklin.com>
2013-12-11 10:06:36 -0800

    flex and bison not needed for the data generator.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Check for all the stuff I need to function.
-for f in gcc flex bison mvn; do
+for f in gcc mvn; do
 	which $f > /dev/null 2>&1
 	if [ $? -ne 0 ]; then
 		echo "Required program $f is missing. Please install it and try again."
