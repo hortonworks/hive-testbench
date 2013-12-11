3334d4cb47491cc9a0798289fc1c7a880bbbbd42
cartershanklin <carter@cartershanklin.com>
2013-12-11 09:53:56 -0800

    No standard option to suppress output, use redirects.

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # Check for all the stuff I need to function.
 for f in gcc flex bison mvn; do
-	which -s $f || ( echo "Required program $f is missing. Please install it and try again." ; exit 1 )
+	which $f > /dev/null 2>&1 || ( echo "Required program $f is missing. Please install it and try again." ; exit 1 )
 done
 
 (cd tpcds-gen; make)
