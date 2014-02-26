27caab60a53bf9cf6eaa5e4ea15559f41bd0b7a3
cartershanklin <carter@cartershanklin.com>
2014-02-26 07:20:46 -0800

    Factor settings into load-partitioned.

--- settings/load-partitioned.sql
+++ settings/load-partitioned.sql
@@ -1,20 +1,23 @@
 set hive.enforce.bucketing=true;
 set hive.enforce.sorting=true;
 set hive.exec.dynamic.partition.mode=nonstrict;
-set hive.exec.max.dynamic.partitions.pernode=1000000;
-set hive.exec.max.dynamic.partitions=1000000;
+set hive.exec.max.dynamic.partitions.pernode=10000;
+set hive.exec.max.dynamic.partitions=10000;
 set hive.exec.max.created.files=1000000;
+set hive.exec.parallel=true;
+set hive.exec.reducers.max=2000;
+set hive.stats.autogather=true;
 
+set mapred.job.reduce.input.buffer.percent=0.0;
 set mapred.min.split.size=240000000;
-set mapred.max.split.size=240000000;
 set mapred.min.split.size.per.node=240000000;
 set mapred.min.split.size.per.rack=240000000;
-set hive.exec.parallel=true;
-set hive.stats.autogather=false;
 
-set mapred.map.child.java.opts=-server -Xmx2248m -Djava.net.preferIPv4Stack=true;
-set mapred.reduce.child.java.opts=-server -Xms1024m -Xmx9000m -Djava.net.preferIPv4Stack=true;
+set mapred.map.child.java.opts=-server -Xmx2500m -Djava.net.preferIPv4Stack=true;
+set mapred.reduce.child.java.opts=-server -Xms1024m -Xmx7900m -Djava.net.preferIPv4Stack=true;
 set mapreduce.map.memory.mb=3072;
-set mapreduce.reduce.memory.mb=12288;
+set mapreduce.reduce.memory.mb=8192;
+
 set io.sort.mb=800;
+
 set hive.optimize.tez=false;
