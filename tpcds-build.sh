bf559481c6b1b93744fb636e496e6a9a081f98f2
Carter Shanklin <carter@HW10204.local>
2014-04-21 17:48:28 -0700

    Check for javac in the path.

--- tpcds-build.sh
+++ tpcds-build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Check for all the stuff I need to function.
-for f in gcc; do
+for f in gcc javac; do
 	which $f > /dev/null 2>&1
 	if [ $? -ne 0 ]; then
-		echo "Required program $f is missing. Please install it and try again."
+		echo "Required program $f is missing. Please install or fix your path and try again."
 		exit 1
 	fi
 done
