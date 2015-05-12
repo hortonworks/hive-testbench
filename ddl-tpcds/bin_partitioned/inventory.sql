b3dd1d97d920a96108711e926f5353874280e0a9
Mostafa Mokhtar <ttmahdy19@hotmail.com>
2015-05-12 12:20:40 -0700

    Partition on inv_date_sk not i.inv_date
    Fix the partitioning scheme

--- ddl-tpcds/bin_partitioned/inventory.sql
+++ ddl-tpcds/bin_partitioned/inventory.sql
@@ -5,21 +5,17 @@ drop table if exists inventory;
 
 create table inventory
 (
-    inv_date_sk         int,
     inv_item_sk          int,
     inv_warehouse_sk		int,
     inv_quantity_on_hand	int
 )
-partitioned by (inv_date string)
+partitioned by (inv_date_sk int)
 stored as ${FILE};
 
-insert overwrite table inventory partition (inv_date)
+insert overwrite table inventory partition (inv_date_sk)
 select
-	i.inv_date_sk,
 	i.inv_item_sk,
 	i.inv_warehouse_sk,
 	i.inv_quantity_on_hand,
-	d.d_date as inv_date
-  from ${SOURCE}.inventory i
-  join ${SOURCE}.date_dim d
-  on (d.d_date_sk = i.inv_date_sk);
+	i.inv_date_sk
+  from ${SOURCE}.inventory i;
