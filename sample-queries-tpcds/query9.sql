d538366d21601d7596caef998436899a5f7c2bcc
Nita Dembla <ndembla@hortonworks.com>
2018-07-23 16:57:47 -0400

    Removing the trivial rewrites and using original tpcds query.

--- sample-queries-tpcds/query9.sql
+++ sample-queries-tpcds/query9.sql
@@ -1,55 +1,51 @@
 -- start query 1 in stream 0 using template query9.tpl and seed 1490436826
-with bucket1q as (
-  select
-  count(*) ct,
-  avg(ss_ext_list_price) alp,
-  avg(ss_net_paid_inc_tax) anp
-  from store_sales where ss_quantity between 1 and 20
-),
-bucket2q as (
-  select
-  count(*) ct,
-  avg(ss_ext_list_price) alp,
-  avg(ss_net_paid_inc_tax) anp
-  from store_sales where ss_quantity between 21 and 40
-),
-bucket3q as (
-  select
-  count(*) ct,
-  avg(ss_ext_list_price) alp,
-  avg(ss_net_paid_inc_tax) anp
-  from store_sales where ss_quantity between 41 and 60
-),
-bucket4q as (
-  select
-  count(*) ct,
-  avg(ss_ext_list_price) alp,
-  avg(ss_net_paid_inc_tax) anp
-  from store_sales where ss_quantity between 61 and 80
-),
-bucket5q as (
-  select
-  count(*) ct,
-  avg(ss_ext_list_price) alp,
-  avg(ss_net_paid_inc_tax) anp
-  from store_sales where ss_quantity between 81 and 100
-)
-select
-       case when bucket1q.ct > 409437
-            then bucket1q.alp
-            else bucket1q.anp end bucket1,
-       case when bucket1q.ct > 4595804
-            then bucket1q.alp
-            else bucket1q.anp end bucket1,
-       case when bucket1q.ct > 7887297
-            then bucket1q.alp
-            else bucket1q.anp end bucket1,
-       case when bucket1q.ct > 10872978
-            then bucket1q.alp
-            else bucket1q.anp end bucket1,
-       case when bucket1q.ct > 43571537
-            then bucket1q.alp
-            else bucket1q.anp end bucket1
-from bucket1q, bucket2q, bucket3q, bucket4q, bucket5q;
+select case when (select count(*) 
+                  from store_sales 
+                  where ss_quantity between 1 and 20) > 409437
+            then (select avg(ss_ext_list_price) 
+                  from store_sales 
+                  where ss_quantity between 1 and 20) 
+            else (select avg(ss_net_paid_inc_tax)
+                  from store_sales
+                  where ss_quantity between 1 and 20) end bucket1 ,
+       case when (select count(*)
+                  from store_sales
+                  where ss_quantity between 21 and 40) > 4595804
+            then (select avg(ss_ext_list_price)
+                  from store_sales
+                  where ss_quantity between 21 and 40) 
+            else (select avg(ss_net_paid_inc_tax)
+                  from store_sales
+                  where ss_quantity between 21 and 40) end bucket2,
+       case when (select count(*)
+                  from store_sales
+                  where ss_quantity between 41 and 60) > 7887297
+            then (select avg(ss_ext_list_price)
+                  from store_sales
+                  where ss_quantity between 41 and 60)
+            else (select avg(ss_net_paid_inc_tax)
+                  from store_sales
+                  where ss_quantity between 41 and 60) end bucket3,
+       case when (select count(*)
+                  from store_sales
+                  where ss_quantity between 61 and 80) > 10872978
+            then (select avg(ss_ext_list_price)
+                  from store_sales
+                  where ss_quantity between 61 and 80)
+            else (select avg(ss_net_paid_inc_tax)
+                  from store_sales
+                  where ss_quantity between 61 and 80) end bucket4,
+       case when (select count(*)
+                  from store_sales
+                  where ss_quantity between 81 and 100) > 43571537
+            then (select avg(ss_ext_list_price)
+                  from store_sales
+                  where ss_quantity between 81 and 100)
+            else (select avg(ss_net_paid_inc_tax)
+                  from store_sales
+                  where ss_quantity between 81 and 100) end bucket5
+from reason
+where r_reason_sk = 1
+;
 
 -- end query 1 in stream 0 using template query9.tpl
