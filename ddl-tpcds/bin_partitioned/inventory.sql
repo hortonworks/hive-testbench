ac6440659a5de9a5faf42e3aa3abfe87a13da7e3
Gopal V <gopalv@apache.org>
2018-10-15 22:07:21 -0700

    Treat inventory as a dimension table

--- ddl-tpcds/bin_partitioned/inventory.sql
+++ ddl-tpcds/bin_partitioned/inventory.sql
@@ -4,18 +4,7 @@ use ${DB};
 drop table if exists inventory;
 
 create table inventory
-(
-    inv_item_sk          bigint,
-    inv_warehouse_sk		bigint,
-    inv_quantity_on_hand	int
-)
-partitioned by (inv_date_sk bigint)
-stored as ${FILE};
-
-insert overwrite table inventory partition (inv_date_sk)
-select
-	i.inv_item_sk,
-	i.inv_warehouse_sk,
-	i.inv_quantity_on_hand,
-	i.inv_date_sk
-  from ${SOURCE}.inventory i;
+stored as ${FILE}
+as select * from ${SOURCE}.inventory
+CLUSTER BY inv_date_sk
+;
