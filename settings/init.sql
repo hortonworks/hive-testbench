8db40ffc1233cbe20ef7066bd3a3d2f3e768cb0a
rbalamohan <rbalamohan@apache.org>
2020-06-26 08:48:36 +0530

    hive.optimize.sort.dynamic.partition.threshold=0

--- settings/init.sql
+++ settings/init.sql
@@ -20,3 +20,5 @@ set hive.convert.join.bucket.mapjoin.tez=false;
 set hive.auto.convert.join.shuffle.max.size=10000000000;
 set hive.tez.llap.min.reducer.per.executor=0.33;
 set hive.map.aggr.hash.min.reduction=0.99;
+
+set hive.optimize.sort.dynamic.partition.threshold=0;
