ab4148def9d89fd503618ca575486b59cb50f2be
Ivan Nikerin <fenderbas666@gmail.com>
2023-02-15 16:44:38 +0300

    add spark in run all

--- run_sql_all.sh
+++ run_sql_all.sh
@@ -2,6 +2,7 @@
 
 TYPE_TEST=$1
 BASE_DATA_STORE=$2
+TYPE_RUNNER=$3
 
 list_sql=($(ls ${TYPE_TEST} | grep sql))
 
@@ -9,7 +10,13 @@ for sql_file in "${list_sql[@]}"
 do
    NUMBER=`echo ${sql_file}| awk -F '.' '{print $1}' | awk -F 'query' '{print $2}'`
    START_TIME=`date +%s`
-   beeline -n hive -u "jdbc:hive2://localhost:10000/${BASE_DATA_STORE}" -f ${TYPE_TEST}/${sql_file}
+   if [ "$TYPE_RUNNER" = "spark2" ]; then
+       spark-sql --num-executors 90 --driver-memory 4g --executor-memory 4g --database ${BASE_DATA_STORE} -f ${TYPE_TEST}/${sql_file}
+   elif [ "$TYPE_RUNNER" = "spark3" ]; then
+       spark3-sql --num-executors 90 --driver-memory 4g --executor-memory 4g --database ${BASE_DATA_STORE} -f ${TYPE_TEST}/${sql_file}
+   elif [ "$TYPE_RUNNER" = "hive" ]; then
+       beeline -n hive -u "jdbc:hive2://localhost:10000/${BASE_DATA_STORE}" -f ${TYPE_TEST}/${sql_file}
+   fi
    EXIT_STATUS=`echo $?`
    if [[ $EXIT_STATUS == 0 ]]; then
        EXIT_STATUS="OK"
