87eadbdcd461defa5f9d229ae68ba84363beaeec
inikerin <nia@arenadata.io>
2024-01-22 18:10:37 +0300

    add test spark syntax

--- sample-queries-tpcds-spark/query95.sql
+++ sample-queries-tpcds-spark/query95.sql
@@ -14,8 +14,7 @@ from
   ,customer_address
   ,web_site
 where
-    d_date between '2002-4-01' and 
-           (cast('2002-4-01' as date) + 60 days)
+    d_date between '2002-4-01' and date_add('2002-4-01', 60)
 and ws1.ws_ship_date_sk = d_date_sk
 and ws1.ws_ship_addr_sk = ca_address_sk
 and ca_state = 'AL'
